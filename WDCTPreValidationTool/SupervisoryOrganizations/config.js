c8895ca6e1f3447d1ad2b3c05e656c07a34a5787
rahuljain1981 <rahuljain@192.168.0.28>
2017-05-22 11:34:43 +0530

    Supervisory Organizaiton Updates

--- WDCTPreValidationTool/SupervisoryOrganizations/config.js
+++ WDCTPreValidationTool/SupervisoryOrganizations/config.js
@@ -1,3 +1,80 @@
+var COUNTRY_ISO_CODE_REGEX = /^(AFG|ALA|ALB|DZA|ASM|AND|AGO|AIA|ATA|ATG|ARG|ARM|ABW|AUS|AUT|AZE|BHS|BHR|BGD|BRB|BLR|BEL|BLZ|BEN|BMU|BTN|BOL|BES|BIH|BWA|BVT|BRA|IOT|VGB|BRN|BGR|BFA|BDI|CPV|KHM|CMR|CAN|CYM|CAF|TCD|CHL|CHN|HKG|MAC|CXR|CCK|COL|COM|COG|COK|CRI|CIV|HRV|CUB|CUW|CYP|CZE|PRK|COD|DNK|DJI|DMA|DOM|ECU|EGY|SLV|GNQ|ERI|EST|ETH|FLK|FRO|FJI|FIN|FRA|GUF|PYF|ATF|GAB|GMB|GEO|DEU|GHA|GIB|GRC|GRL|GRD|GLP|GUM|GTM|GGY|GIN|GNB|GUY|HTI|HMD|VAT|HND|HUN|ISL|IND|IDN|IRN|IRQ|IRL|IMN|ISR|ITA|JAM|JPN|JEY|JOR|KAZ|KEN|KIR|KWT|KGZ|LAO|LVA|LBN|LSO|LBR|LBY|LIE|LTU|LUX|MDG|MWI|MYS|MDV|MLI|MLT|MHL|MTQ|MRT|MUS|MYT|MEX|FSM|MCO|MNG|MNE|MSR|MAR|MOZ|MMR|NAM|NRU|NPL|NLD|NCL|NZL|NIC|NER|NGA|NIU|NFK|MNP|NOR|OMN|PAK|PLW|PAN|PNG|PRY|PER|PHL|PCN|POL|PRT|PRI|QAT|KOR|MDA|REU|ROU|RUS|RWA|BLM|SHN|KNA|LCA|MAF|SPM|VCT|WSM|SMR|STP|SAU|SEN|SRB|SYC|SLE|SGP|SXM|SVK|SVN|SLB|SOM|ZAF|SGS|SSD|ESP|LKA|PSE|SDN|SUR|SJM|SWZ|SWE|CHE|SYR|TJK|THA|MKD|TLS|TGO|TKL|TON|TTO|TUN|TUR|TKM|TCA|TUV|UGA|UKR|ARE|GBR|TZA|UMI|USA|VIR|URY|UZB|VUT|VEN|VNM|WLF|ESH|YEM|ZMB|ZWE)$/i;
+
+var WDCT_Validator = {
+   validatingColumn: "0",
+   columns: {
+	   "1" : {
+		      "VALIDATIONS" : {
+		    	              "find-blank" :{
+		    	            	      "TYPE" : "REGEX"
+		    	            	      ,"VALIDATIONRULE" : /^\s*$/
+			    	                  ,"MSG" : "Value cannot be blank"
+			    	                  ,"MENUITEMLABEL" : "Find Blank Values"
+			    	                  ,"LOOKUPINDEX": ""	  
+		                           },
+		                       "find-dup" : {
+		                    	    
+		                    	          "TYPE" : "DUPLICATE"
+			    	            	      ,"VALIDATIONRULE" : /^\s*$/
+				    	                  ,"MSG" : "Value cannot be dublicate"
+				    	                  ,"MENUITEMLABEL" : "Find Duplicate Values"
+				    	                  ,"LOOKUPINDEX": ""
+		                           }    
+		      }
+		     
+	   },
+	   "4" : {
+		      "VALIDATIONS" : {
+		    	              "find-blank" :{
+		    	            	      "TYPE" : "REGEX"
+		    	            	      ,"VALIDATIONRULE" : /^\s*$/
+			    	                  ,"MSG" : "Value cannot be blank"
+			    	                  ,"MENUITEMLABEL" : "Find Blank Values"
+			    	                  ,"LOOKUPINDEX": ""	  
+		                           },
+		                           "find-lookup-sheet-value" : {
+									   "TYPE" : "JSFUNCTION_LOOKUP"
+									   ,"VALIDATIONRULE" : "VALIDATE_LOOUPSHEETDATA"
+									   ,"MSG" : "Value should be in related sheet"
+			    	                   ,"MENUITEMLABEL" : "Find invalid not in original sheet"
+			    	                   ,"LOOKUPINDEX": "" 	   
+									}
+                     
+		      }
+		     
+	   }
+		
+   }	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var WDCT_LookUpConfig = {
+		"SUPERVISORY ORGANIZATIONS" : {
+			"4": {
+				"sheet" : "LOCATIONS",
+				"col": 1
+			}
+		}
+}
+
+
+
+/** NOT NEEDED ANY MORE - REMOVE AFTER USING BELOW REGEX FOR ACTUAL VALIDATORS ***/
+
+
+/**
 var WDCT_REGEX = {
 		"MM/DD/YYYY" : /^((0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2})*$/
 		,"NUMERICAL" : /^\d+$/
@@ -21,73 +98,26 @@ var WDCT_REGEX = {
 		,"EMPL_ID" : ""
 		,"CURRENCY" : /^(AED|AFN|ALL|AMD|ANG|AOA|ARS|AUD|AWG|AZN|BAM|BBD|BDT|BGN|BHD|BIF|BMD|BND|BOB|BOV|BRL|BSD|BTN|BWP|BYR|BZD|CAD|CDF|CHE|CHF|CHW|CLF|CLP|CNY|COP|COU|CRC|CUC|CUP|CVE|CZK|DJF|DKK|DOP|DZD|EGP|ERN|ETB|EUR|FJD|FKP|GBP|GEL|GHS|GIP|GMD|GNF|GTQ|GYD|HKD|HNL|HRK|HTG|HUF|IDR|ILS|INR|IQD|IRR|ISK|JMD|JOD|JPY|KES|KGS|KHR|KMF|KPW|KRW|KWD|KYD|KZT|LAK|LBP|LKR|LRD|LSL|LTL|LVL|LYD|MAD|MDL|MGA|MKD|MMK|MNT|MOP|MRO|MUR|MVR|MWK|MXN|MXV|MYR|MZN|NAD|NGN|NIO|NOK|NPR|NZD|OMR|PAB|PEN|PGK|PHP|PKR|PLN|PYG|QAR|RON|RSD|RUB|RWF|SAR|SBD|SCR|SDG|SEK|SGD|SHP|SLL|SOS|SRD|SSP|STD|SVC|SYP|SZL|THB|TJS|TMT|TND|TOP|TRY|TTD|TWD|TZS|UAH|UGX|USD|USN|USS|UYI|UYU|UZS|VEF|VND|VUV|WST|XAF|XAG|XAU|XBA|XBB|XBC|XBD|XCD|XDR|XFU|XOF|XPD|XPF|XPT|XSU|XTS|XUA|XXX|YER|ZAR|ZMW|ZWL)$/
 }
-
-
+**/
+/**
 var WDCT_HEADER_REGEX = {
 		"EMPLOYEE'S STATUS: ACTIVE, TERMINATED, ON LEAVE" : /(Active|Terminated|On Leave)$/i,
 		"COUNTRY ISO CODE" : /^(AFG|ALA|ALB|DZA|ASM|AND|AGO|AIA|ATA|ATG|ARG|ARM|ABW|AUS|AUT|AZE|BHS|BHR|BGD|BRB|BLR|BEL|BLZ|BEN|BMU|BTN|BOL|BES|BIH|BWA|BVT|BRA|IOT|VGB|BRN|BGR|BFA|BDI|CPV|KHM|CMR|CAN|CYM|CAF|TCD|CHL|CHN|HKG|MAC|CXR|CCK|COL|COM|COG|COK|CRI|CIV|HRV|CUB|CUW|CYP|CZE|PRK|COD|DNK|DJI|DMA|DOM|ECU|EGY|SLV|GNQ|ERI|EST|ETH|FLK|FRO|FJI|FIN|FRA|GUF|PYF|ATF|GAB|GMB|GEO|DEU|GHA|GIB|GRC|GRL|GRD|GLP|GUM|GTM|GGY|GIN|GNB|GUY|HTI|HMD|VAT|HND|HUN|ISL|IND|IDN|IRN|IRQ|IRL|IMN|ISR|ITA|JAM|JPN|JEY|JOR|KAZ|KEN|KIR|KWT|KGZ|LAO|LVA|LBN|LSO|LBR|LBY|LIE|LTU|LUX|MDG|MWI|MYS|MDV|MLI|MLT|MHL|MTQ|MRT|MUS|MYT|MEX|FSM|MCO|MNG|MNE|MSR|MAR|MOZ|MMR|NAM|NRU|NPL|NLD|NCL|NZL|NIC|NER|NGA|NIU|NFK|MNP|NOR|OMN|PAK|PLW|PAN|PNG|PRY|PER|PHL|PCN|POL|PRT|PRI|QAT|KOR|MDA|REU|ROU|RUS|RWA|BLM|SHN|KNA|LCA|MAF|SPM|VCT|WSM|SMR|STP|SAU|SEN|SRB|SYC|SLE|SGP|SXM|SVK|SVN|SLB|SOM|ZAF|SGS|SSD|ESP|LKA|PSE|SDN|SUR|SJM|SWZ|SWE|CHE|SYR|TJK|THA|MKD|TLS|TGO|TKL|TON|TTO|TUN|TUR|TKM|TCA|TUV|UGA|UKR|ARE|GBR|TZA|UMI|USA|VIR|URY|UZB|VUT|VEN|VNM|WLF|ESH|YEM|ZMB|ZWE)$/i
 }
-
-
-var WDCT_LookUpConfig = {
-	"EMPLOYEE DATA" : {
-		"6": {
-			"sheet" : "SUPERVISORY ORGANIZATIONS",
-			"col": 1
-		},
-		"7": {
-			"sheet" : "SUPERVISORY ORGANIZATIONS",
-			"col": 2
-		},
-		"8": {
-			"sheet" : "LOCATIONS",
-			"col": 1
-		},
-		"9": {
-			"sheet" : "COMPANY",
-			"col": 1
-		},
-		"10": {
-			"sheet" : "COMPANY",
-			"col": 2
-		},
-		"11": {
-			"sheet" : "COST CENTERS",
-			"col": 2
-		},
-		"12": {
-			"sheet" : "COST CENTERS",
-			"col": 1
-		},
-		"14": {
-			"sheet" : "REGIONS",
-			"col": 2
-		},
-		"132": {
-			"sheet" : "POSITIONS",
-			"col": 7
-		},
-		"134": {
-			"sheet" : "JOB PROFILES",
-			"col": 1
-		},
-		"135": {
-			"sheet" : "JOB PROFILES",
-			"col": 2
-		},
-		"144": {
-			"sheet" : "PAY GROUPS",
-			"col": 1
-		},
-		"145": {
-			"sheet" : "PAY GROUPS",
-			"col": 2
-		}
-	}
-}
+**/
 
 
 var WDCT_RELATEDVALIDATIONS = {
+	"4" : {
+	   "LOOKUPINDEX": "252",
+	   "TYPE" : "DATECOMPARE",
+	   "VALIDATIONRULE" : "LESSTHAN"   
+	}, 
+	"252" : {
+		   "LOOKUPINDEX": "4",
+		   "TYPE" : "DATECOMPARE",
+		   "VALIDATIONRULE" : "GREATERTHAN"   
+		},
 	"33" : {
 		"LOOKUPINDEX" : "32",
 		"TYPE" : "MAP",
